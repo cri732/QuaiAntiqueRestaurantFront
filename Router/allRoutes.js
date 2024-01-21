b83cdf160f6abe3647c5fc10f38d29809f2de1cd
cri <cri@cri.fr>
2024-01-21 22:14:14 +0100

    Ajout page galerie + Mise à jour des liens vers galerie dans index et home

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 // Définir ici les routes
 export const allRoutes = [
-	new Route("/", "Accueil", "/pages/home.html")
+	new Route("/index.html", "Accueil", "/pages/home.html"),
+	new Route("/", "Accueil", "/pages/home.html"),
+	new Route("/galerie", "Galerie", "/pages/galerie.html")
 ];
 
 export const websiteName = "Quai Antique";
