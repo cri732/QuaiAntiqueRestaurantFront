c3a91646b2ba4ec97c76b8742f2f56d005d55362
cri <cri@cri.fr>
2024-01-22 22:31:17 +0100

    Ajout page connexion + Ajout page inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
 	new Route("/index.html", "Accueil", "/pages/home.html"),
 	new Route("/", "Accueil", "/pages/home.html"),
-	new Route("/galerie", "Galerie", "/pages/galerie.html")
+	new Route("/galerie", "Galerie", "/pages/galerie.html"),
+	new Route("/signin", "Connexion", "/pages/signin.html"),
+	new Route("/signup", "Inscription", "/pages/signup.html")
 ];
 
 export const websiteName = "Quai Antique";
